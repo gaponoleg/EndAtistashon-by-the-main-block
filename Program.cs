8595b53bc55182ee4d5bdf5bfbb5004545195fba
Oleg <gapon.oleg999@gmail.com>
2024-01-20 18:21:15 +0500

    задали array из строк

--- Program.cs
+++ Program.cs
@@ -8,3 +8,5 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] array = {"Hello", "2", "world", ":-)"};
