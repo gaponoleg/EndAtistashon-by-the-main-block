6826935a963e70e730fa00cefce6776a28047d9d
Oleg <gapon.oleg999@gmail.com>
2024-01-20 21:10:30 +0500

    создали решение задачи

--- Program.cs
+++ Program.cs
@@ -9,4 +9,21 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
-string[] array = {"Hello", "2", "world", ":-)"};
+string[] inputArray = {"Hello", "2", "world", ":-)"};
+List<string> outputList = new List<string>(); //Создали list типа string
+
+foreach (string str in inputArray) //Цикл который находит из массива строк строку с 3 или менее симвалов
+{
+    if (str.Length <= 3)
+    {
+        outputList.Add(str);
+    }
+}
+
+string[] outputArray = outputList.ToArray(); //Конвертирует list в масcив типа string
+
+Console.WriteLine("\nНовый массив строк (длина <= 3 символов):"); //Выводит новый массив типа string
+foreach (string str in outputArray)
+{
+    Console.Write(str + " ");
+}
